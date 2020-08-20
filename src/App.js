8824ec8650661d7a079007c242a27efe42e9c41e
gregory harris <gregoryharris@gregorys-mbp.attlocal.net>
2020-08-20 16:58:28 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+// import { Route } from "react-router-dom";
+
+// import Navigation from "./components/navigation/Navigation";
+// import Welcome from "./components/welcome/Welcome";
+// import Clock from "./components/clock/Clock";
+// import Contact from "./components/contact/Contact";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navigation />
+
+//       <Route exact path="/" render={() => <Welcome name="Greg" />} />
+
+//       <Route path="/contact" component={Contact} />
+//       <Route path="/clock" component={Clock} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Welcome from "./components/welcome/Welcome";
+import Clock from "./components/clock/Clock";
+import Contact from "./components/contact/Contact";
+import Navigation from "./components/navigation/Navigation";
+import Page404 from "./components/page404/Page404";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+
+      <Switch>
+        <Route
+          path="/welcome/:name"
+          render={(props) => (
+            <Welcome {...props} name={props.match.params.name} />
+          )}
+        />
+
+        <Route
+          exact
+          path="/"
+          render={(props) => <Welcome {...props} name="Greg" />}
+        />
+
+        <Route path="/Clock" component={Clock} />
+        <Route path="/Contact" component={Contact} />
+        <Route component={Page404} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/page404/Page404.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFound(props) {
+  return (
+    <div>
+      <h3>404 page not found</h3>
+      <p>Page Not Found</p>
+    </div>
+  );
+}
+
+export default NotFound;
